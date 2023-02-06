9f727dbf6267ad33d60ebfb2f467495427c021bd
Lisacyj0102 <s1110169@my.cmsh.cyc.edu.tw>
2023-02-06 19:31:45 +0800

    add Arm

--- src/main/java/frc/robot/component/Arm.java
+++ src/main/java/frc/robot/component/Arm.java
@@ -1,6 +1,14 @@
 package frc.robot.component;
 
 public class Arm {
+    public static final double encoderPulse = 4096;
+    public static final double gearing = 512;
     public static void init() {}
-    public static void teleop() {}
+    public static void teleop() {} 
+    public static double positionToDistanceMeter(double position){
+            double sensorRate = position/encoderPulse;
+            double wheelRate = sensorRate/gearing;
+            double positionMeter=wheelRate*360; 
+            return positionMeter;
+        }
 }
