d82cba0d42b6ea78582b0b422a0fa1541cfe071e
Lisacyj0102 <s1110169@my.cmsh.cyc.edu.tw>
2023-02-07 19:53:05 +0800

    add arm

--- src/main/java/frc/robot/component/Arm.java
+++ src/main/java/frc/robot/component/Arm.java
@@ -1,14 +1,34 @@
 package frc.robot.component;
 
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.Robot;
+
 public class Arm {
-    public static final double encoderPulse = 4096;
-    public static final double gearing = 512;
-    public static void init() {}
-    public static void teleop() {} 
+    private static final double encoderPulse = 4096;
+    private static final double gearing = 198;
+    private static CANSparkMax armmotor ;//
+    private static RelativeEncoder armencoder;
+    private static int karm = 0;
+
+
+    public static void init() {
+    armmotor = new CANSparkMax(karm, MotorType.kBrushless);
+    }
+    public static void teleop() {
+        //get degree position
+        double a = positionToDegreeMeter(armencoder.getPosition());
+        //
+        armmotor.set(Robot.xbox.getLeftTriggerAxis());
+        armmotor.set(-Robot.xbox.getRightTriggerAxis());
+    } 
     public static double positionToDegreeMeter(double position){
             double sensorRate = position/encoderPulse;
-            double wheelRate = sensorRate/gearing;
-            double positionMeter=wheelRate*360; 
+            double armRate = sensorRate/gearing;
+            double positionMeter=armRate*360; 
             return positionMeter;
         }
 }
