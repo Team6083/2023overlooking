39212d95ea3aa8f2338e78bb61e1c229a76cef9b
weiru8787878787 <weiru960331@gmail.com>
2023-02-16 19:07:11 +0800

    revert arm

--- src/main/java/frc/robot/component/Arm.java
+++ src/main/java/frc/robot/component/Arm.java
@@ -2,7 +2,6 @@ package frc.robot.component;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.REVLibError;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.controller.PIDController;
@@ -24,7 +23,7 @@ public class Arm {
     private static final int karm2 = 0;
     private static final int line = 2;
     private static Double rotateForward;
-    private static Double rotatereverse;
+    private static Double rotateReverse;
     private static RelativeEncoder ArmEncoder;
     private static Encoder lineEncoder;
     private static Double kP = 0.0;
@@ -45,30 +44,29 @@ public class Arm {
     public static void teleop() {
         // rotate arm
         rotateForward = Robot.xbox.getLeftTriggerAxis() - Robot.xbox.getRightTriggerAxis();
-        rotatereverse = Robot.xbox.getRightTriggerAxis() - Robot.xbox.getLeftTriggerAxis();
+        rotateReverse = Robot.xbox.getLeftTriggerAxis() - Robot.xbox.getRightTriggerAxis();
         ArmMotorleft.set(rotateForward);
-        ArmMotorright.set(rotatereverse);
+        ArmMotorright.set(rotateReverse);
+
+        double angle = positionToDegree(ArmEncoder.getPosition());// get the angular position
+        double length = positionTolength(lineEncoder.get()); // get length position
 
         // take up and pay off device
         if (Robot.xbox.getPOV() == 0) {
             lineMotor.set(0.5);
         } else if (Robot.xbox.getPOV() == 180) {
             lineMotor.set(-0.5);
+        } else if (Robot.xbox.getAButton()) {
+            if (length > 186) {
+                lineMotor.set(-0.5);
+            } else if (length < 186) {
+                lineMotor.set(0.5);
+            } else {
+                lineMotor.set(0);
+            }
         } else {
             lineMotor.set(0);
         }
-        
-        double Armdegree = ArmEncoder.getPosition();
-        double goal = ArmEncoder.(goal);
-
-        ArmPID.setSetpoint(goal);;
-
-        double ArmVolt = ArmPID.calculate();
-
-        Arm.setVoltage(ArmVolt);
-
-        double angle = positionToDegreeMeter(ArmEncoder.getPosition());// get the angular position
-        double length = positionTolengthMeter(lineEncoder.get()); // get length position
 
         if (Robot.xbox.getAButton()) {
             if (angle > 35) {
@@ -81,19 +79,14 @@ public class Arm {
                 ArmMotorleft.set(0);
                 ArmMotorright.set(0);
             }
-
-            if (length > 186) {
-                lineMotor.set(-0.5);
-            } else if (length < 186) {
-                lineMotor.set(0.5);
-            } else {
-                lineMotor.set(0);
-            }
         }
+
+        double point = 
+
     }
 
     // do the number of turns calculate(to a particular angle)
-    public static double positionToDegreeMeter(double turns) {
+    public static double positionToDegree(double turns) {
         double sensorRate = turns / ArmencoderPulse;
         double armRate = sensorRate / Armgearing;
         double DegreeMeter = armRate * 360;
@@ -101,7 +94,7 @@ public class Arm {
     }
 
     // do the number of turns calculate(to a particular length)
-    public static double positionTolengthMeter(double position) {
+    public static double positionTolength(double position) {
         double sensorRate = position / lineencoderPulse;
         double armRate = sensorRate / linegearing;
         double lengthMeter = armRate;
@@ -114,7 +107,7 @@ public class Arm {
     }
 
     public static double autoAccessDegree() {
-        double Degree = positionToDegreeMeter(ArmEncoder.getPosition());
+        double Degree = positionTolength(ArmEncoder.getPosition());
         return Degree;
     }
 
