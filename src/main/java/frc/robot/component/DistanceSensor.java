c0b723b5e6d98c5a01ff5ba2c57e3a9cb7a0a2e8
William960321 <s1110270@my.cmsh.cyc.edu.tw>
2023-03-04 11:42:08 +0800

    distance sensor

--- src/main/java/frc/robot/component/DistanceSensor.java
+++ src/main/java/frc/robot/component/DistanceSensor.java
@@ -3,12 +3,18 @@ package frc.robot.component;
 import com.revrobotics.Rev2mDistanceSensor;
 import com.revrobotics.Rev2mDistanceSensor.Port;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class DistanceSensor {
     public static Rev2mDistanceSensor distSens;
 
     public static void init() {
         distSens = new Rev2mDistanceSensor(Port.kOnboard);
         distSens.setAutomaticMode(true);
+    }
+
+    public static void teleop() {
+        SmartDashboard.putNumber("null", distSens.GetRange() * 2.54);
     }
 
 }
