79256d944a3513e3770f297beabc3a33965edf70
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2023-03-19 03:06:35 +0800

    modify drivebase output ratio by arm angle

--- src/main/java/frc/robot/component/DriveBase.java
+++ src/main/java/frc/robot/component/DriveBase.java
@@ -106,7 +106,9 @@ public class DriveBase {
     // Normal drivebase
     public static void teleop() {
 
-        double outputRatio = (Robot.mainController.getLeftBumper() || Robot.mainController.getRightBumper()) ? 1 : 0.9;
+        double outputRatio = (Robot.mainController.getLeftBumper() || Robot.mainController.getRightBumper()) ? 1 : 0.7;
+        outputRatio *= 1 - ((Math.abs(90 - Robot.arm.getAngleDegree()) / 90.0) * 0.4);
+
         leftMotorSpeedInput = Robot.mainController.getLeftY() * outputRatio;
         rightMotorSpeedInput = Robot.mainController.getRightY() * outputRatio;
 
